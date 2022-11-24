1836595768ab3fc296ff33725ac3cac8f7021ce0
ibu2661 <64191247+ibu2661@users.noreply.github.com>
2022-11-24 21:06:47 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,9 +4,9 @@
 // キャッシュ名とキャッシュファイルの指定
 var CACHE_NAME = 'pwa-test-caches';
 var urlsToCache = [
-	'/pwa/',
-	'/pwa/css/style.css',
-	'/pwa/drawer.js'
+	'/',
+	'/css/style.css',
+	'/drawer.js'
 ];
 
 // インストール処理
