c8a400e80735192aa212c421fde30f2327f9fe6d
ibu2661 <64191247+ibu2661@users.noreply.github.com>
2022-11-28 20:02:53 +0900

    Update drawer.js

--- drawer.js
+++ drawer.js
@@ -3,10 +3,14 @@ let clearButton; //消すボタン
 
 //キャンバスの設定
 function setup() {
-	canvas = createCanvas(600, 600);//Canvasを作成
+	canvas = createCanvas();//Canvasを作成
 	background(255); //Canvasの背景を白にする
 	clearButton = createButton('消す');//ボタンを作成
 	clearButton.mousePressed(clearCanvas);//ボタンクリックの関数を指定
+	var w = $('.wrapper').width();
+	var h = $('.wrapper').height();
+	$('#canvas').attr('width', w);
+	$('#canvas').attr('height', h);
 }
 
 //マウスで絵を描くための関数
